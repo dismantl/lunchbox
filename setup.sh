8f45930cd0a6254053c509064401e6d0037436d1
Dan Staples <dan@acab.enterprises>
2021-11-18 17:07:01 -0500

    Split up playbook based on remote user

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/lunchbox
+if [ ! -f .initialized ]; then
+    ansible-playbook -i hosts init.yml
+    touch .initialized
+fi
 ansible-playbook -i hosts setup.yml "$@"
